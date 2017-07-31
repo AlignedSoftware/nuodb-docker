165abcd96f4b9b2e58147626709c3bf527d47a80
ben higgins <bhiggins@nuodb.com>
2017-07-31 14:00:53 -0400

    fixed capitalization

--- scripts/startNuodb.sh
+++ scripts/startNuodb.sh
@@ -100,7 +100,7 @@ fi
 /opt/nuodb/etc/nuorestsvc start
 
 #create and populate database if doesn't exist
-if [ "${NODE_TYPE}" == "TE" ] && ${POPUlATE_NUODB}; then
+if [ "${NODE_TYPE}" == "TE" ] && ${POPULATE_NUODB}; then
     #check if there's a db already
     eval_db="$(/opt/nuodb/bin/nuodbmgr --broker localhost --password $DOMAIN_PASSWORD --command "show domain databases" | grep ${DB_NAME})"
     if [ -z "$eval_db" ]; then
